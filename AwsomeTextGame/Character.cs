655dcd62bd3351a661781255c9fe761353e88328
Desirée Wenner Persson <desiree.persson@hotmail.com>
2017-04-23 16:49:21 +0200

    c#  är elakt xD

---
+++ AwsomeTextGame/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AwsomeTextGame
+{
+    public class Character
+    {
+
+        private int char_id = 2;
+        private string name = "Börje";
+
+
+        public void move()
+        {
+            Console.WriteLine("Moved");
+        }
+
+
+    }
+
+}
+
--- AwsomeTextGame/Program.cs
+++ AwsomeTextGame/Program.cs
@@ -6,11 +6,46 @@ using System.Threading.Tasks;
 
 namespace AwsomeTextGame
 {
-    class Program
+     class Program
     {
         static void Main(string[] args)
         {
+            
+            Character newChar = new Character();
+            
+            int char_id = 3;            
+            string name;
+            
+            //skapar ett random värde mellan 1 - 31 och lägger det i char_id
+            //Random id = new Random();
+            //char_id = id.Next(1,31);
+            
             Console.WriteLine("Hej!");
+            Console.WriteLine(" ");               
+                                   
+            Console.WriteLine("Fyll i karaktärens namn: ");
+            name = Console.ReadLine();
+
+
+
+
+            Console.WriteLine(" ");    
+            Console.WriteLine("Karaktärens id är: " + char_id);
+            Console.WriteLine("Karaktärens namn är: " + name);
+            newChar.move();
+
+            
+
+            // Keep the console window open in debug mode.   
+            Console.WriteLine(" ");    
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
+
+
